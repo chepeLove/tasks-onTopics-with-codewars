6310981b245781902ad1a8ab3a6b7187d9c2f654
chepeLove <ilya.chepelov@gmail.com>
2024-02-29 14:36:06 +0300

    feat: add task Reversed Strings

---
+++ loopsAndPrimitives/reversedStrings.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it reverses the string passed into it.
+//
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+const reversedStrings = (str) => str.split('').reverse().join('')
+
+console.log(reversedStrings('world'))
